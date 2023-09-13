e929cb887e6cdda320e4999f22cc4479868e9d61
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-13 16:39:56 -0700

    main implemented

--- PracticeQuestion_1.java
+++ PracticeQuestion_1.java
@@ -21,4 +21,14 @@ public class PracticeQuestion_1 {
 	 * 				RETURN i
 	 */
 	
+	public int findMissingNumber(int[] nums) {
+
+	}
+
+	public static void main(String[] args) {
+		PracticeQuestion_1 pq1 = new PracticeQuestion_1();
+
+		System.out.println(pq1.findMissingNumber(new int[] { 3, 7, 1, 2, 8, 4, 5 }));
+		System.out.println(pq1.findMissingNumber(new int[]{13, 3, 7, 11, 4, 6, 2, 10, 9, 5, 8, 1}));
+	}
 }

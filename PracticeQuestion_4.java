5afd276f2106518246646bb56ca5f2fde6dc8313
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-14 17:26:51 -0700

    main implemented

--- PracticeQuestion_4.java
+++ PracticeQuestion_4.java
@@ -17,17 +17,26 @@ public class PracticeQuestion_4 {
 		Map<Integer, Integer> map = new HashMap<>();
 		int[] ans = new int[2];
 		for (int i = 0; i < nums.length; i++) {
-			int comp = target-i;
-			if (!map.containsKey(comp))
-				map.put(comp, i);
-			else if (map.containsKey(nums[i])) {
+			int comp = target - nums[i];
+			
+			if (map.containsKey(nums[i])) {
 				ans[0] = map.get(nums[i]);
 				ans[1] = i;
+				break;
 			}
-				
+			if (!map.containsKey(comp))
+				map.put(comp, i);
+
 		}
-		
+
 		return ans;
+	}
+	
+	public static void main(String[] args) {
+		PracticeQuestion_4 pq4 = new PracticeQuestion_4();
+
+		System.out.println(Arrays.toString(pq4.uniqueSum(new int[] { 2, 7, 11, 15 }, 9)));
+		System.out.println(Arrays.toString(pq4.uniqueSum(new int[] {3, 6, 2, 4, 7, 8, 10}, 13)));
 	}
 	 
 }
